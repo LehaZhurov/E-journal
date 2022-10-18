e4f4d3870abffc4d63112b830a5dacb51c040273
AlexeiZhurov <zhurov@greensight.ru>
2022-10-18 20:40:03 +0300

    Поправил косяк

--- resources/js/teacher/reports/createReport.js
+++ resources/js/teacher/reports/createReport.js
@@ -2,8 +2,10 @@ import { SendRequest } from '../../SendRequest.js';
 import { Alert } from '../../Alert.js';
 import { getReport } from './getReport';
 
-export function createReport(data) {
-    SendRequest('POST', 'create/report', data)
+export function createReport(id) {
+    let form = document.querySelector(id);
+    form = new FormData(form);
+    SendRequest('POST', 'create/report', form)
         .then(data => Alert('Отчет готов', 'success'))//Передаем сообщение от сервера
         .catch(err => Alert('Что то пошло не так', 'error'));
     getReport('#report_form');
